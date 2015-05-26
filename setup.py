1eff385446ce4b144e56aed0376cce71ce40c6d8
Nikolaos Korasidis <renelvon@gmail.com>
2015-05-27 00:28:47 +0300

    Fix and expand setup module.
    * Pyroma 9/10

--- setup.py
+++ setup.py
@@ -1,33 +1,38 @@
-from codecs import open
+"""Setup module for txrudp."""
+
+import codecs
 from os import path
 
 from setuptools import find_packages, setup
 
 
-if __name__ == '__main__':
-    here = path.abspath(path.dirname(__file__))
-    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
+_HERE = path.abspath(path.dirname(__file__))
+with codecs.open(path.join(_HERE, 'README.md'), encoding='utf-8') as f:
+    _LONG_DESCRIPTION = f.read()
 
-    setup(
-        name='txrudp',
-        version='0.1.0',
-        description='A Twisted extension implementing RUDP',
-        long_description=long_description,
-        url='https://github.com/Renelvon/txrudp',
-        author='Nikolaos Korasidis',
-        author_email='renelvon@gmail.com',
-        license='MIT',
-        classifiers=(
-            'Development Status :: 2 - Pre-Alpha',
-            'Framework :: Twisted',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 2.7',
-            'Topic :: System :: Networking'
-        ),
-        keywords=('rudp', 'twisted', 'reliable'),
-        packages=find_packages(exclude=('tests',)),
-        install_requires=('twisted', 'jsonschema'),
-    )
+setup(
+    name='txrudp',
+    version='0.1.0',
+    description='A Twisted extension implementing RUDP',
+    long_description=_LONG_DESCRIPTION,
+    url='https://github.com/Renelvon/txrudp',
+    author='Nikolaos Korasidis',
+    author_email='renelvon@gmail.com',
+    license='MIT',
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Networking'
+    ),
+    keywords='rudp twisted reliable',
+    packages=find_packages(exclude=('tests',)),
+    install_requires=('twisted', 'jsonschema'),
+    extras_require={
+        'dev': ('prospector',)
+    },
+    zip_safe=False
+)
