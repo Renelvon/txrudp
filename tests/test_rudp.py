47459bd127731462c8bb62ca34a60eba67023f22
Nikolaos Korasidis <renelvon@gmail.com>
2015-06-07 01:45:41 +0300

    Test packet reception.
    * Refine mocks used.

--- tests/test_rudp.py
+++ tests/test_rudp.py
@@ -4,8 +4,7 @@ import logging
 import unittest
 
 import mock
-from twisted.internet import protocol
-from twisted.test import proto_helpers
+from twisted.internet import address, protocol, udp
 
 from txrudp import connection, heap, packet, rudp
 
@@ -34,6 +33,7 @@ class TestConnectionManagerAPI(unittest.TestCase):
         self.assertIsInstance(cm, protocol.DatagramProtocol)
         self.assertIsInstance(cm, collections.MutableMapping)
         self.assertEqual(cm.public_ip, self.public_ip)
+        self.assertIsNone(cm.port)
         self.assertFalse(cm.relaying)
         self.assertEqual(len(cm), 0)
 
@@ -46,6 +46,7 @@ class TestConnectionManagerAPI(unittest.TestCase):
             logger=logging.Logger('CM')
         )
         self.assertEqual(cm.public_ip, self.public_ip)
+        self.assertIsNone(cm.port)
         self.assertTrue(cm.relaying)
 
     def test_get_nonexistent_connection(self):
@@ -119,7 +120,9 @@ class TestConnectionManagerAPI(unittest.TestCase):
 
     def _make_connected_cm(self):
         cm = self._make_cm()
-        transport = mock.Mock()
+        transport = mock.Mock(spec_set=udp.Port)
+        ret_val = address.IPv4Address('UDP', self.public_ip, self.port)
+        transport.attach_mock(mock.Mock(return_value=ret_val), 'getHost')
         cm.makeConnection(transport)
         return cm
 
@@ -163,6 +166,77 @@ class TestConnectionManagerAPI(unittest.TestCase):
         self.assertNotIn((dest_ip, 12345), cm)
         cm.transport.write.assert_called_once_with(datagram, (dest_ip, 12345))
         cm.connection_factory.make_new_connection.assert_not_called()
+
+    def test_receive_datagram_in_existing_connection(self):
+        cm = self._make_connected_cm()
+
+        source_addr = self.addr3
+        mock_connection = cm.make_new_connection(
+            (self.public_ip, self.port),
+            source_addr
+        )
+        cm[source_addr] = mock_connection
+        rudp_packet = packet.RUDPPacket(
+            1,
+            self.public_ip,
+            self.port,
+            source_addr[0],
+            source_addr[1]
+        )
+        datagram = json.dumps(rudp_packet.to_json())
+
+        cm.datagramReceived(datagram, source_addr)
+        cm.connection_factory.make_new_connection.assert_not_called()
+        mock_connection.receive_packet.assert_called_once_with(rudp_packet)
+
+    def test_receive_datagram_in_new_connection(self):
+        cm = self._make_connected_cm()
+
+        source_addr = self.addr3
+        rudp_packet = packet.RUDPPacket(
+            1,
+            self.public_ip,
+            self.port,
+            source_addr[0],
+            source_addr[1]
+        )
+        datagram = json.dumps(rudp_packet.to_json())
+
+        cm.datagramReceived(datagram, source_addr)
+        cm.connection_factory.make_new_connection.assert_called_once_with(
+            cm,
+            (self.public_ip, self.port),
+            source_addr,
+            source_addr
+        )
+        self.assertIn(source_addr, cm)
+        mock_connection = cm[source_addr]
+        mock_connection.receive_packet.assert_called_once_with(rudp_packet)
+
+    def test_receive_datagram_in_new_relayed_connection(self):
+        cm = self._make_connected_cm()
+
+        source_addr = self.addr3
+        relay_addr = self.addr3
+        rudp_packet = packet.RUDPPacket(
+            1,
+            self.public_ip,
+            self.port,
+            source_addr[0],
+            source_addr[1]
+        )
+        datagram = json.dumps(rudp_packet.to_json())
+
+        cm.datagramReceived(datagram, relay_addr)
+        cm.connection_factory.make_new_connection.assert_called_once_with(
+            cm,
+            (self.public_ip, self.port),
+            source_addr,
+            relay_addr
+        )
+        self.assertIn(source_addr, cm)
+        mock_connection = cm[source_addr]
+        mock_connection.receive_packet.assert_called_once_with(rudp_packet)
 
     def test_make_new_connection(self):
         cm = self._make_cm()
