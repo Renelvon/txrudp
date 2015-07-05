c146bdd6e0ea53377cb4c275b23afb6e56c0c3c6
Nikolaos Korasidis <renelvon@gmail.com>
2015-07-05 17:48:08 +0300

    Replace JSON API with protobuf API.

--- txrudp/packet.py
+++ txrudp/packet.py
@@ -3,96 +3,45 @@ Specification of RUDP packet structure.
 
 Classes:
     Packet: An RUDP packet implementing a total ordering and
-        serializing to/from JSON.
+        serializing to/from protobuf.
 """
 
 import functools
+import re
 
-import jsonschema
+import packet_pb2
 
-# NOTE: jsonschema's `format` specifier was tested and found
-# lacking. Hence the use of regexes from Regular Expressions Cookbook.
+# IP validation regexes from the Regular Expressions Cookbook.
 # For now, only standard (non-compressed) IPv6 addresses are
 # supported. This might change in the future.
 _IPV4_REGEX = r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
 _IPV6_REGEX = r'^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$'
 
-RUDP_PACKET_JSON_SCHEMA = {
-    '$schema': 'http://json-schema.org/schema#',
-    'id': 'RUDP_PACKET_JSON_SCHEMA',
-    'type': 'object',
-    'properties': {
-        'sequence_number': {
-            'type': 'integer',
-            'minimum': 0
-        },
-        'dest_ip': {
-            'anyOf': [
-                {'type': 'string', 'pattern': _IPV4_REGEX},
-                {'type': 'string', 'pattern': _IPV6_REGEX}
-            ]
-        },
-        'dest_port': {
-            'type': 'integer',
-            'minimum': 1,
-            'maximum': 65535
-        },
-        'source_ip': {
-            'anyOf': [
-                {'type': 'string', 'pattern': _IPV4_REGEX},
-                {'type': 'string', 'pattern': _IPV6_REGEX}
-            ]
-        },
-        'source_port': {
-            'type': 'integer',
-            'minimum': 1,
-            'maximum': 65535
-        },
-        'payload': {
-            'type': 'string',
-            'default': ''
-        },
-        'more_fragments': {
-            'type': 'integer',
-            'minimum': 0,
-            'default': 0
-        },
-        'ack': {
-            'type': 'integer',
-            'minimum': 0,
-            'default': 0
-        },
-        'fin': {
-            'type': 'boolean',
-            'default': False
-        },
-        'syn': {
-            'type': 'boolean',
-            'default': False
-        },
-    },
-    'additionalProperties': False,
-    'required': [
-        'sequence_number',
-        'dest_ip',
-        'dest_port',
-        'source_ip',
-        'source_port',
-        'payload',
-        'ack',
-        'fin',
-        'syn',
-        'more_fragments'
-    ],
-}
+_IP_MATCHER = re.compile('({0})|({1})'.format(_IPV4_REGEX, _IPV6_REGEX))
+
+
+class ValidationError(Exception):
+
+    """Exception raised due to invalid data (e.g. bad IP)."""
 
 
 @functools.total_ordering
 class Packet(object):
 
-    """An RUDP packet."""
+    """
+    An RUDP packet.
 
-    def __init__(
+    This class serves as a wrapper class for the protobuf
+    serialization class. The wrapping is necessary in order to
+    enable extra functionality (e.g. total ordering) without
+    disrupting the metaclass of the protobuf class.
+    """
+
+    def __init__(self):
+        """Create a new empty Packet."""
+        self._packet = packet_pb2.Packet()
+
+    def populate(
         self,
         sequence_number,
         dest_addr,
@@ -104,7 +53,8 @@ class Packet(object):
         syn=False,
     ):
         """
-        Create a new packet with the given fields.
+        Populate the Packet with the given fields. This will
+        overwrite eny existing values.
 
         Args:
             seqnum: The packet's sequence number, as an int.
@@ -124,14 +74,52 @@ class Packet(object):
         first recipient of this packet in case where the recipient is
         a mediator node relaying packet (e.g. for NAT punching).
         """
+        self.syn = syn
+        self.fin = fin
         self.sequence_number = sequence_number
-        self.dest_addr = dest_addr
-        self.source_addr = source_addr
-        self.payload = payload
         self.more_fragments = more_fragments
         self.ack = ack
-        self.fin = fin
-        self.syn = syn
+
+        self.payload = payload
+
+        self.dest_addr = dest_addr
+        self.source_addr = source_addr
+
+    def to_bytes(self):
+        """
+        Return a serialized version of this packet.
+
+        Returns:
+            A protobuf-encoded bytestring.
+
+        Raises:
+            protobuf.EncodeError: Serialization was unsuccessful;
+                maybe the object attributes have not been instatiated
+                with proper values.
+        """
+        return self._packet.SerializeToString()
+
+    @classmethod
+    def from_bytes(cls, data):
+        """
+        Create a Packet from an unvalidated bytestring.
+
+        Args:
+            data: A protobuf-encoded bytestring.
+
+        Returns:
+            A new Packet instance, populated with the contents
+            of the bytestring.
+
+        Raises:
+            protobuf.DecodeError: Decoding the bytestring into
+                Packet was unsuccessful.
+            ValidationError: One or more values was invalid.
+        """
+        new_packet = cls()
+        new_packet._packet.ParseFromString(data)
+        cls.validate(new_packet)
+        return new_packet
 
     def __eq__(self, other):
         if isinstance(other, Packet):
@@ -145,84 +133,164 @@ class Packet(object):
         else:
             return NotImplemented
 
-    def to_json(self):
-        """
-        Create a JSON object representing this packet.
-
-        Returns:
-            A JSON object that should be valid against
-            RUDP_PACKET_JSON_SCHEMA.
-        """
-        return {
-            'sequence_number': self.sequence_number,
-            'dest_ip': self.dest_addr[0],
-            'dest_port': self.dest_addr[1],
-            'source_ip': self.source_addr[0],
-            'source_port': self.source_addr[1],
-            'payload': self.payload,
-            'more_fragments': self.more_fragments,
-            'ack': self.ack,
-            'fin': self.fin,
-            'syn': self.syn
-        }
-
-    @classmethod
-    def from_unvalidated_json(cls, json_obj):
-        """
-        Create a Packet from an unvalidated json_obj.
-
-        Args:
-            json_obj: An RUDP packet in JSON format.
-
-        Returns:
-            An RUDPMesssage instance representing the same packet.
-
-        Raises:
-            jsonschema.ValidationError: The packet was invalid.
-        """
-        cls.validate(json_obj)
-        return cls.from_validated_json(json_obj)
-
-    @classmethod
-    def from_validated_json(cls, json_obj):
-        """
-        Create a Packet from a validated json_obj.
-
-        Args:
-            json_obj: An RUDP packet in JSON format.
-
-        Returns:
-            An RUDPMesssage instance representing the same packet.
-        """
-        sequence_number = int(json_obj['sequence_number'])
-        dest_addr = (json_obj['dest_ip'], int(json_obj['dest_port']))
-        source_addr = (json_obj['source_ip'], int(json_obj['source_port']))
-        payload = json_obj['payload']
-        more_fragments = json_obj['more_fragments']
-        ack = int(json_obj['ack'])
-        fin = bool(json_obj['fin'])
-        syn = bool(json_obj['syn'])
-
-        return cls(
-            sequence_number,
-            dest_addr,
-            source_addr,
-            payload,
-            more_fragments,
-            ack,
-            fin,
-            syn
-        )
-
     @staticmethod
     def validate(packet):
         """
-        Test the packet against RUDP_PACKET_JSON_SCHEMA.
+        Ensure packet values are valid.
 
         Args:
-            packet: The packet to validate, as a JSON object.
+            packet: The Packet to validate.
 
         Raises:
-            jsonschema.ValidationError: The packet was invalid.
+            ValidationError: One or more values was invalid.
         """
-        jsonschema.validate(packet, RUDP_PACKET_JSON_SCHEMA)
+        dest_ip, dest_port = packet.dest_addr
+        if _IP_MATCHER.match(dest_ip) is None:
+            raise ValidationError(
+                'Bad destination IP: {0}.'.format(dest_ip)
+            )
+
+        if not 1 <= dest_port <= 65535:
+            raise ValidationError(
+                'Bad destination port: {0}.'.format(dest_port)
+            )
+
+        source_ip, source_port = packet.source_addr
+        if _IP_MATCHER.match(source_ip) is None:
+            raise ValidationError(
+                'Bad source IP: {0}.'.format(source_ip)
+            )
+
+        if not 1 <= source_port <= 65535:
+            raise ValidationError(
+                'Bad source port: {0}.'.format(source_port)
+            )
+
+    def get_syn(self):
+        return self._packet.syn
+
+    def set_syn(self, value):
+        """
+        Set the Packet's SYN flag.
+
+        Args:
+            value: True or False.
+
+        Raises:
+            google.protobuf.TypeError: Value has inappropriate type.
+        """
+        self._packet.syn = value
+
+    def get_fin(self):
+        return self._packet.fin
+
+    def set_fin(self, value):
+        """
+        Set the Packet's FIN flag.
+
+        Args:
+            value: True or False.
+
+        Raises:
+            google.protobuf.TypeError: Value has inappropriate type.
+        """
+        self._packet.fin = value
+
+    def get_sequence_number(self):
+        return self._packet.sequence_number
+
+    def set_sequence_number(self, value):
+        """
+        Set the Packet's sequence number.
+
+        Args:
+            value: A non-negative integer.
+
+        Raises:
+            google.protobuf.TypeError: Value has inappropriate type.
+        """
+        self._packet.sequence_number = value
+
+    def get_more_fragments(self):
+        return self._packet.more_fragments
+
+    def set_more_fragments(self, value):
+        """
+        Set the number of fragments following this Packet.
+
+        Args:
+            value: A non-negative integer.
+
+        Raises:
+            google.protobuf.TypeError: Value has inappropriate type.
+        """
+        self._packet.more_fragments = value
+
+    def get_ack(self):
+        return self._packet.ack
+
+    def set_ack(self, value):
+        """
+        Set the Packet's acknowledgement number.
+
+        Args:
+            value: A non-negative integer.
+
+        Raises:
+            google.protobuf.TypeError: Value has inappropriate type.
+        """
+        self._packet.ack = value
+
+    def get_payload(self):
+        return self._packet.payload
+
+    def set_payload(self, value):
+        """
+        Set the Packet's payload.
+
+        Args:
+            value: Packet's payload, in bytes.
+
+        Raises:
+            google.protobuf.TypeError: Value has inappropriate type.
+        """
+        self._packet.payload = value
+
+    def get_dest_addr(self):
+        return (self._packet.dest_ip, self._packet.dest_port)
+
+    def set_dest_addr(self, value):
+        """
+        Set the Packet's destination address.
+
+        Args:
+            value: An (IP, port) tuple.
+
+        Raises:
+            google.protobuf.TypeError: Value has inappropriate type.
+        """
+        self._packet.dest_ip, self._packet.dest_port = value
+
+    def get_source_addr(self):
+        return (self._packet.source_ip, self._packet.source_port)
+
+    def set_source_addr(self, value):
+        """
+        Set the Packet's source address.
+
+        Args:
+            value: An (IP, port) tuple.
+
+        Raises:
+            google.protobuf.TypeError: Value has inappropriate type.
+        """
+        self._packet.source_ip, self._packet.source_port = value
+
+    syn = property(get_syn, set_syn)
+    fin = property(get_fin, set_fin)
+    sequence_number = property(get_sequence_number, set_sequence_number)
+    more_fragments = property(get_more_fragments, set_more_fragments)
+    ack = property(get_ack, set_ack)
+    payload = property(get_payload, set_payload)
+    dest_addr = property(get_dest_addr, set_dest_addr)
+    source_addr = property(get_source_addr, set_source_addr)
