ca9f96af3902f5bfadc89bb8b71f45cb3950e694
Nikolaos Korasidis <nkorasid@student.ethz.ch>
2020-01-27 18:04:16 +0100

    Make setup configuration more declarative

--- setup.py
+++ setup.py
@@ -12,31 +12,10 @@ with codecs.open(path.join(_HERE, "README.rst"), encoding="utf-8") as f:
     _LONG_DESCRIPTION = f.read()
 
 setup(
-    name="txrudp",
     version="0.5.2",
-    description="A Twisted extension implementing RUDP",
     long_description=_LONG_DESCRIPTION,
-    url="https://github.com/OpenBazaar/txrudp",
-    author="Nikolaos Korasidis",
-    author_email="renelvon@gmail.com",
-    license="MIT",
-    classifiers=(
-        "Development Status :: 4 - Beta",
-        "Framework :: Twisted",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: System :: Networking",
-    ),
-    keywords="rudp twisted reliable",
     packages=("txrudp", "tests"),
-    install_requires=("enum34", "protobuf", "twisted"),
     extras_require={"crypto": ("pynacl",)},
-    tests_require=("check-manifest>=0.38", "coverage", "nose", "mock"),
     test_suite="nose.collector",
     include_package_data=True,
-    zip_safe=False,
 )
